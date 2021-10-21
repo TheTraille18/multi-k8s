66f57cbcae85b7e8ad841e5956a3d353f9b3c18f
TheTraille18 <jtraille07@gmail.com>
2021-10-21 00:32:29 -0400

    client-deployment -> client-deployments

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ docker push jtraille07/multi-server
 docker push jtraille07/multi-worker
 kubectl apply -f k8s/
 kubectl rollout restart deployments/server-deployment
-kubectl rollout restart deployments/client-deployment
+kubectl rollout restart deployments/client-deployments
 kubectl rollout restart deployments/worker-deployment
